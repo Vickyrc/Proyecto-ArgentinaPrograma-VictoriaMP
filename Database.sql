3ce0fae5b6600e6b5723ccc5819aea8d7bf4abd3
Victoria MartinezP <99889499+Vickyrc@users.noreply.github.com>
2022-06-28 20:22:01 -0300

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,231 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for macos12 (x86_64)
+--
+-- Host: localhost    Database: portfolioweb
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `domicilio`
+--
+
+DROP TABLE IF EXISTS `domicilio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `domicilio` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `calle` varchar(70) DEFAULT NULL,
+  `altura` varchar(45) DEFAULT NULL,
+  `piso` varchar(45) DEFAULT NULL,
+  `depto` varchar(45) DEFAULT NULL,
+  `codigo_postal` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domicilio`
+--
+
+LOCK TABLES `domicilio` WRITE;
+/*!40000 ALTER TABLE `domicilio` DISABLE KEYS */;
+INSERT INTO `domicilio` VALUES (1,'Rivadavia','2080','8','B','2000');
+/*!40000 ALTER TABLE `domicilio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `educacion`
+--
+
+DROP TABLE IF EXISTS `educacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `educacion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `institucion` varchar(80) DEFAULT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `id_persona` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_educacion_persona1_idx` (`id_persona`),
+  CONSTRAINT `fk_educacion_persona1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `educacion`
+--
+
+LOCK TABLES `educacion` WRITE;
+/*!40000 ALTER TABLE `educacion` DISABLE KEYS */;
+INSERT INTO `educacion` VALUES (1,'Colegio Edmondo de Amicis','Primario','1999-03-01','2006-12-10',1),(2,'Colegio Edmondo de Amicis','Secundario','2006-03-01','2010-12-10',1),(3,'Universidad Catolica Argentina','Abogada','2011-03-01','2016-12-22',1),(4,'I.C.R','Curso de Asistente Juridico','2014-06-01','2014-10-22',1),(5,'Universidad Nacional del Litoral','Posgrado en derecho de familia','2018-02-01','2018-12-22',1);
+/*!40000 ALTER TABLE `educacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `experiencia_laboral`
+--
+
+DROP TABLE IF EXISTS `experiencia_laboral`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `experiencia_laboral` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_empresa` varchar(45) DEFAULT NULL,
+  `es_trabajo_actual` tinyint DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `id_persona` int NOT NULL,
+  `id_tipo_empleo` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_experiencia_laboral_persona_idx` (`id_persona`),
+  KEY `fk_experiencia_laboral_tipo_empleo1_idx` (`id_tipo_empleo`),
+  CONSTRAINT `fk_experiencia_laboral_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`),
+  CONSTRAINT `fk_experiencia_laboral_tipo_empleo1` FOREIGN KEY (`id_tipo_empleo`) REFERENCES `tipo_empleo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `experiencia_laboral`
+--
+
+LOCK TABLES `experiencia_laboral` WRITE;
+/*!40000 ALTER TABLE `experiencia_laboral` DISABLE KEYS */;
+INSERT INTO `experiencia_laboral` VALUES (1,'Estudio Juridico Vitta',0,'2014-08-01','2016-09-01','Secretaria de estudio juridico\natencion telefonica, redaccion de escritos, tramites en tribunales',1,2);
+/*!40000 ALTER TABLE `experiencia_laboral` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `fecha_nac` datetime DEFAULT NULL,
+  `telefono` varchar(12) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `sobre_mi` varchar(200) DEFAULT NULL,
+  `url_foto` varchar(100) DEFAULT NULL,
+  `id_domicilio` int NOT NULL,
+  `id_usuarios` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_persona_domicilio1_idx` (`id_domicilio`),
+  KEY `fk_persona_usuarios1_idx` (`id_usuarios`),
+  CONSTRAINT `fk_persona_domicilio1` FOREIGN KEY (`id_domicilio`) REFERENCES `domicilio` (`id`),
+  CONSTRAINT `fk_persona_usuarios1` FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` VALUES (1,'Victoria','Martinez Pintado','1992-11-22 00:00:00','3413431277','vickycrr@hotmail.com','Soy abogada, actualmente estoy incursionando en la programación. Soy proactiva y me gusta trabajar\nen equipo','www.foto.com/fotovicky',1,1);
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proyectos`
+--
+
+DROP TABLE IF EXISTS `proyectos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proyectos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_proyecto` varchar(60) DEFAULT NULL,
+  `id_persona` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_proyectos_persona1_idx` (`id_persona`),
+  CONSTRAINT `fk_proyectos_persona1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proyectos`
+--
+
+LOCK TABLES `proyectos` WRITE;
+/*!40000 ALTER TABLE `proyectos` DISABLE KEYS */;
+INSERT INTO `proyectos` VALUES (1,'Proyecto Final argentina Programa',1);
+/*!40000 ALTER TABLE `proyectos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_empleo`
+--
+
+DROP TABLE IF EXISTS `tipo_empleo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipo_empleo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_tipo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_empleo`
+--
+
+LOCK TABLES `tipo_empleo` WRITE;
+/*!40000 ALTER TABLE `tipo_empleo` DISABLE KEYS */;
+INSERT INTO `tipo_empleo` VALUES (1,'Full time'),(2,'Part time'),(3,'Emprendimiento');
+/*!40000 ALTER TABLE `tipo_empleo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'VictoriaMP','vickycrr@hotmail.com');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-28 15:51:42
